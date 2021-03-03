3b090b051bbf27e50f8b4c507a734dad9059aa78
Stacy Betts <stacyo23@yahoo.com>
2021-03-03 17:47:17 -0600

    finished header + header styling

---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+
+
+const Header = () => {
+    return ( 
+        <div className="container">
+        
+        <h1>Employee Directory</h1>
+        <br></br>
+        <h4>Click on the carrot to filter by heading or use the search box to narrow your results.</h4>
+        <hr/>
+        </div>
+        
+        
+
+     );
+}
+ 
+export default Header;
