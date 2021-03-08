947e9fe339e8194ad4347d3af7ff089d7b1a56bc
Stacy Betts <stacyo23@yahoo.com>
2021-03-07 21:04:57 -0600

    added search form component

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Header from "./components/Header"
+import SearchForm from "./components/SearchForm"
+
 
 function App() {
   return (
+    <div>
     <Header/>
+    <SearchForm/>
+    </div>
   );
 }
 
---
+++ employee-directory/src/components/SearchForm/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const SearchForm = () => {
+    return ( 
+       <form className="search">
+           <label htmlFor="Search"
+           ></label>
+       </form> 
+     );
+}
+ 
+export default SearchForm;
